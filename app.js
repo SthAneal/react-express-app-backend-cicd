15da10f3030fed2ed0c8df733a53b1520c2ff2a2
sthAneal <anilmeronaam@gmail.com>
2022-05-29 15:14:27 +1000

    route defined with /api/

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname,'/frontend/build')));
 
-app.get('/', (req, res)=>{
+app.get('/api', (req, res)=>{
     res.send('default route').status(200);
 });
 
-app.get('/home', (req, res)=>{
+app.get('/api/home', (req, res)=>{
     res.send('home route').status(201);
 });
 
-app.get('/dashboard', (req, res)=>{
+app.get('/api/dashboard', (req, res)=>{
     res.send('dashboard route').status(200);
 });
 
