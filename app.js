226a15bddb40973f235ed9a01432799db6f043cc
sthAneal <anilmeronaam@gmail.com>
2022-05-29 14:57:54 +1000

    status added to the response

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const path = require('path');
 app.use(express.static(path.join(__dirname,'/frontend/build')));
 
 app.get('/', (req, res)=>{
-    res.send('default route');
+    res.send('default route').status(200);
 });
 
 app.get('/home', (req, res)=>{
-    res.send('home route');
+    res.send('home route').status(201);
 });
 
 app.get('/dashboard', (req, res)=>{
-    res.send('dashboard route');
+    res.send('dashboard route').status(200);
 });
 
 app.listen(3000, ()=>{
