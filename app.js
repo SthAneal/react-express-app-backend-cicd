8d1ae2a60b5d989b97cd8f7a34ebc0ea339d6e7e
sthAneal <anilmeronaam@gmail.com>
2022-05-29 13:18:05 +1000

    express server setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,'/frontend/build')));
+
+app.get('/', (req, res)=>{
+    res.send('default route');
+});
+
+app.get('/home', (req, res)=>{
+    res.send('home route');
+});
+
+app.get('/dashboard', (req, res)=>{
+    res.send('dashboard route');
+});
+
+app.listen(5000, ()=>{
+    console.log('server is running on port 5000');
+});
