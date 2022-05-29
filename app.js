fe0ad3a6adee7a6aac806814fa1f1636584127a3
sthAneal <anilmeronaam@gmail.com>
2022-05-29 19:09:52 +1000

    res json added

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const app = express();
 // app.use(express.static(path.join(__dirname,'/frontend/build')));
 
 app.get('/api', (req, res)=>{
-    res.send({msg:'default user', status:200});
+    res.json({msg:'default user', status:200});
 });
 
 app.get('/api/home', (req, res)=>{
     // res.send('home user').status(200);
-    res.send({msg:'home user', status:200});
+    res.json({msg:'home user', status:200});
 });
 
 app.get('/api/dashboard', (req, res)=>{
     // res.send('dashboard route').status(200);
-    res.send({msg:'dashboard user', status:200});
+    res.json({msg:'dashboard user', status:200});
 });
 
 app.listen(3000, ()=>{
